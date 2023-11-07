7544f8c3c5dc10ab9a9e5e879bbd4d0ad9796bc1
Will Wagner <github@willwagner.me>
2023-11-06 16:52:36 -0800

    update

--- EditTalents.py
+++ EditTalents.py
@@ -1,6 +1,8 @@
 from IcarusEdit import get_characters, find_character_index_by_name, save_characters, get_max_talents
 
+CHARACTER_NAME = "BODICUS"
+
 chars = get_characters()
-target_char = find_character_index_by_name("BODICUS", chars)
+target_char = find_character_index_by_name(CHARACTER_NAME, chars)
 chars[target_char].Talents = get_max_talents()
 save_characters(chars)
